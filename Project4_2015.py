7b5085053c32a097a6e46e123ea0da66e9e69175
Tylor Thatcher <66439943+FormulaTranslator@users.noreply.github.com>
2020-06-15 10:24:31 -0500

    random updates

--- Project4_2015.py
+++ Project4_2015.py
@@ -1,16 +1,36 @@
-import hashlib
-
+from hashlib import md5
 project_input = 'yzbqklnj'
 number_to_add = 0
 match_found = False
 
-while not match_found:
+while True:
     secret_code = project_input + str(number_to_add)
-    hash_result = hashlib.md5(secret_code.encode())
+    hash_result = md5(secret_code.encode())
     hex_result = hash_result.hexdigest()
-    if hex_result.startswith('000000'):
+    if hex_result.startswith('00000'):
         print(number_to_add)
         print(hex_result)
-        match_found = True
+        break
 
     number_to_add += 1
+
+
+# Mark's scalable alternative way:
+from itertools import count
+
+
+def solve(password, prefix='00000', numbers_to_add=count()):
+    for number_to_add in numbers_to_add:
+        secret_code = password + str(number_to_add)
+        hash_result = md5(secret_code.encode())
+        hex_result = hash_result.hexdigest()
+        if hex_result.startswith(prefix):
+            return number_to_add, hex_result
+
+
+password_vars = solve('yzbqklnj')
+print(*password_vars, sep='\n')
+
+password_vars = solve('yzbqklnj', '000000')
+print(*password_vars, sep='\n')
+
