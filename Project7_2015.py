781df91493555a31aebf25552772bd87ea0bc5bd
Tylor Thatcher <66439943+FormulaTranslator@users.noreply.github.com>
2020-06-30 16:21:26 -0500

    Simplified Day 7
    Went from 118 lines to 74 by using proper string handling and lambdas in a dictionary

--- Project7_2015.py
+++ Project7_2015.py
@@ -1,118 +1,74 @@
+# For part 2, 1674 -> b was changed to 46065 -> b
 import Bit16_FUNCS as b16
 
-max_value = 65535
 wire_dictionary = {'operators completed': 0}
 file_name = 'Project7_2015'
-
-
+operation_instructions = []
+operations = {
+    'NOT': lambda signal, place_holder: b16.BinNot(signal),
+    'AND': lambda signal1, signal2: b16.BinAnd(signal1, signal2),
+    'OR': lambda signal1, signal2: b16.BinOr(signal1, signal2),
+    'RSHIFT': lambda signal1, signal2: b16.BinRshift(signal1, signal2),
+    'LSHIFT': lambda signal1, signal2: b16.BinLshift(signal1, signal2)
+}
 with open(file_name) as file:
-    number_lines = file.read().count('\n')
+    for line in file:
+        operation_instructions.append(line.rstrip('\n'))
 
-while wire_dictionary['operators completed'] < number_lines+1:
-    # operators_complete = 0
-    with open(file_name) as file:
-        for index, line in enumerate(file):
-            if line[len(line)-1] == '\n':
-                end_of_line = -1
-            else:
-                end_of_line = None
+number_lines = len(operation_instructions)
 
-            if 'NOT' in line:
-                from_signal = line[line.find(' ')+1:line.find('->')-1]
-                to_signal = line[line.find('->') + 3:end_of_line]
-                if from_signal in wire_dictionary and to_signal not in wire_dictionary:
-                    wire_dictionary[to_signal] = b16.BinNot(wire_dictionary[from_signal])
+while wire_dictionary['operators completed'] < number_lines:
+    for index, instruction in enumerate(operation_instructions):
+        # Define initial variables
+        variables = instruction.split()
+        number_variables = len(variables)
+        to_signal = variables[-1]
+        from_signal_is_defined = False
+        second_signal_is_defined = False
+
+        # Assign dependant variables from input
+        if number_variables == 3:  # No command has only 3 segments
+            from_signal = variables[0]
+            operation = None
+            second_signal = None
+        elif number_variables == 4:  # NOT command has only 4 segments
+            from_signal = variables[1]
+            operation = variables[0]
+            second_signal = None
+        else:  # All other commands have 5 segments
+            from_signal = variables[0]
+            operation = variables[1]
+            second_signal = variables[2]
+
+        # Define from signal if possible (None will be True to enable operation of NOT operations)
+        if from_signal.isnumeric():
+            from_signal = int(from_signal)
+            from_signal_is_defined = True
+        elif from_signal in wire_dictionary:
+            from_signal = wire_dictionary[from_signal]
+            from_signal_is_defined = True
+        # Define second signal if possible (None will be True to enable operation of NOT operations)
+        if second_signal is None:
+            second_signal_is_defined = True
+        elif second_signal.isnumeric():
+            second_signal = int(second_signal)
+            second_signal_is_defined = True
+        elif second_signal in wire_dictionary:
+            second_signal = wire_dictionary[second_signal]
+            second_signal_is_defined = True
+
+        if operation is None:
+            # Simple assign if not operation is given
+            if to_signal not in wire_dictionary:
+                if from_signal_is_defined:
+                    wire_dictionary[to_signal] = from_signal
                     wire_dictionary['operators completed'] += 1
-                    if wire_dictionary[to_signal] > max_value:
-                        wire_dictionary[to_signal] = max_value
+        else:
+            # call function based off of operation given
+            if from_signal_is_defined and second_signal_is_defined and to_signal not in wire_dictionary:
+                wire_dictionary[to_signal] = operations[operation](*[from_signal, second_signal])
+                wire_dictionary['operators completed'] += 1
 
-            elif ' OR ' in line:
-                from_signal1 = line[:line.find(' ')]
-                from_signal2 = line[line.find('OR') + 3:line.find('->') - 1]
-                to_signal = line[line.find('->') + 3:end_of_line]
-                if from_signal1.isalpha() and from_signal2.isalpha() and to_signal not in wire_dictionary:
-                    if from_signal1 in wire_dictionary and from_signal2 in wire_dictionary and to_signal not in wire_dictionary:
-                        wire_dictionary[to_signal] = b16.BinOr(wire_dictionary[from_signal1], wire_dictionary[from_signal2])
-                        wire_dictionary['operators completed'] += 1
-                        if wire_dictionary[to_signal] > max_value:
-                            wire_dictionary[to_signal] = max_value
-                elif from_signal1.isalpha():
-                    if from_signal1 in wire_dictionary and to_signal not in wire_dictionary:
-                        wire_dictionary[to_signal] = b16.BinOr(wire_dictionary[from_signal1], int(from_signal2))
-                        wire_dictionary['operators completed'] += 1
-                        if wire_dictionary[to_signal] > max_value:
-                            wire_dictionary[to_signal] = max_value
-                elif from_signal2.isalpha():
-                    if from_signal2 in wire_dictionary and to_signal not in wire_dictionary:
-                        wire_dictionary[to_signal] = b16.BinOr(int(from_signal1), wire_dictionary[from_signal2])
-                        wire_dictionary['operators completed'] += 1
-                        if wire_dictionary[to_signal] > max_value:
-                            wire_dictionary[to_signal] = max_value
-
-            elif 'AND' in line:
-                from_signal1 = line[:line.find(' ')]
-                from_signal2 = line[line.find('AND') + 4:line.find('->') - 1]
-                to_signal = line[line.find('->') + 3:end_of_line]
-                if from_signal1.isalpha() and from_signal2.isalpha() and to_signal not in wire_dictionary:
-                    if from_signal1 in wire_dictionary and from_signal2 in wire_dictionary and to_signal not in wire_dictionary:
-                        wire_dictionary[to_signal] = b16.BinAnd(wire_dictionary[from_signal1], wire_dictionary[from_signal2])
-                        wire_dictionary['operators completed'] += 1
-                        if wire_dictionary[to_signal] > max_value:
-                            wire_dictionary[to_signal] = max_value
-                elif from_signal1.isalpha() and to_signal not in wire_dictionary:
-                    if from_signal1 in wire_dictionary and to_signal not in wire_dictionary:
-                        wire_dictionary[to_signal] = b16.BinAnd(wire_dictionary[from_signal1], int(from_signal2))
-                        wire_dictionary['operators completed'] += 1
-                        if wire_dictionary[to_signal] > max_value:
-                            wire_dictionary[to_signal] = max_value
-                elif from_signal2.isalpha() and to_signal not in wire_dictionary:
-                    if from_signal2 in wire_dictionary and to_signal not in wire_dictionary:
-                        wire_dictionary[to_signal] = b16.BinAnd(int(from_signal1), wire_dictionary[from_signal2])
-                        wire_dictionary['operators completed'] += 1
-                        if wire_dictionary[to_signal] > max_value:
-                            wire_dictionary[to_signal] = max_value
-
-            elif 'LSHIFT' in line:
-                from_signal = line[:line.find(' ')]
-                shift_number = int(line[line.find('LSHIFT') + 7:line.find('->') - 1])
-                to_signal = line[line.find('->') + 3:end_of_line]
-                if to_signal == 'w':
-                    test = 1
-                if from_signal in wire_dictionary and to_signal not in wire_dictionary:
-                    wire_dictionary[to_signal] = b16.BinLshift(wire_dictionary[from_signal], shift_number)
-                    wire_dictionary['operators completed'] += 1
-                    if wire_dictionary[to_signal] > max_value:
-                        wire_dictionary[to_signal] = max_value
-
-            elif 'RSHIFT' in line:
-                from_signal = line[:line.find(' ')]
-                shift_number = int(line[line.find('RSHIFT')+7:line.find('->') - 1])
-                to_signal = line[line.find('->') + 3:-1]
-                if from_signal in wire_dictionary and to_signal not in wire_dictionary:
-                    wire_dictionary[to_signal] = b16.BinRshift(wire_dictionary[from_signal], shift_number)
-                    wire_dictionary['operators completed'] += 1
-                    if wire_dictionary[to_signal] > max_value:
-                        wire_dictionary[to_signal] = max_value
-
-            elif line[0].isalpha():
-                from_signal = line[:line.find(' ')]
-                to_signal = line[line.find('->') + 3:end_of_line]
-                if from_signal in wire_dictionary and to_signal not in wire_dictionary:
-                    wire_dictionary[to_signal] = wire_dictionary[from_signal]
-                    wire_dictionary['operators completed'] += 1
-                    if wire_dictionary[to_signal] > max_value:
-                        wire_dictionary[to_signal] = max_value
-
-            else:
-                value = int(line[:line.find(' ')])
-                to_signal = line[line.find('->') + 3:end_of_line]
-                if to_signal not in wire_dictionary:
-                    wire_dictionary[to_signal] = value
-                    wire_dictionary['operators completed'] += 1
-                    if wire_dictionary[to_signal] > max_value:
-                        wire_dictionary[to_signal] = max_value
-print(wire_dictionary)
+# print(wire_dictionary)
 if 'a' in wire_dictionary:
     print(wire_dictionary['a'])
-if 'or' in wire_dictionary:
-    print(wire_dictionary['or'])
